9ece166a3fdb5a7f03b27d4521baedb0a6e8f12b
nouhaila <nouhaila.elfahsi20@gmail.com>
2022-09-12 00:51:49 +0100

    secong commit

---
+++ Backend/src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.model.MyUserDetails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONObject;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:3000" , allowCredentials = "true")
+public class AuthController {
+
+    private static Logger log = LoggerFactory.getLogger(AuthController.class);
+
+    @GetMapping("/login")
+    public ResponseEntity<String> home(Authentication auth) {
+        MyUserDetails myUserDetails = (MyUserDetails) auth.getPrincipal();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("role", myUserDetails.getAuthorities());
+        return new ResponseEntity<String>(jsonObject.toString(), HttpStatus.OK);
+    }
+
+}
